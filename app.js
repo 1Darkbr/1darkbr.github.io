edcdbc1a6ac342384a4c7768da793e9e4fb80745
1Dark <71292510+1Darkbr@users.noreply.github.com>
2022-09-13 16:13:43 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+  document.body.classList.toggle('Wtheme')
+
+  var className = document.body.className;
+  if (className == "Btheme") {
+       this.textContent = "🌚";
+  }
+  else{
+    this.textContent = "🌞";
+  }
+
+  console.log('current class name: ' + className);
+});
